96763fa4483e693cbc97c5ad769aeeec65ce0086
vcheckzen <18008498+vcheckzen@users.noreply.github.com>
2021-12-14 17:21:18 +0800

    refactor(build): simplify code

--- build.cmd
+++ build.cmd
@@ -4,30 +4,37 @@ if exist dist (
     del /s /q dist\*
 )
 
-set CGO_ENABLED=0
 set GOARCH=amd64
+call :build-desktop
 
+set GOARCH=arm64
+call :build-desktop
+
+call :build-mobile
+
+exit 0
+
+:compile
+go build -v -o dist\np_%GOOS%_%GOARCH%%1 -trimpath -ldflags "-s -w -buildid="
+exit /b
+
+:build-desktop
+set CGO_ENABLED=0
 set GOOS=windows
 call :compile .exe
 
 set GOOS=linux
 call :compile
 
-set GOARCH=arm64
-call :compile
-
 set GOOS=darwin
 call :compile
+exit /b
 
+:build-mobile
 set TOOLCHAIN=C:\Users\LOGI\AppData\Local\Android\Sdk\ndk\android-ndk-r21b\toolchains\llvm\prebuilt\windows-x86_64
 set CC=%TOOLCHAIN%\bin\armv7a-linux-androideabi16-clang.cmd
 set CGO_ENABLED=1
-set GOARCH=arm
 set GOOS=android
+set GOARCH=arm
 call :compile
-
-exit
-
-:compile
-go build -v -o dist\np_%GOOS%_%GOARCH%%1 -trimpath -ldflags "-s -w -buildid="
 exit /b
